a26db50df90fa66043f6d7ba87f623b8ba15468b
Alex Boutin <alex@cinetic.ca>
2021-03-13 14:12:46 -0500

    restructuring

--- Models/Grid.js
+++ Models/Grid.js
@@ -1,12 +1,16 @@
+const className = "c-board"
+const baseTable = $(`<table class="${className} ${className}-table"></table>`)
+const baseBody = $(`<tbody class="${className} ${className}-tbody"></tbody>`)
 
 class Grid {
     /**
-     * Create the base instance of the Grid. call generate() to return the composed grid.
+     * Create the base instance of the Grid. call build() to return the composed grid.
      * @param {object[]} data
      * @param {string[] || GridHeader[]} fields - Headers' name and property associated
      * @param {int} [take=10] - How many items per page
+     * @param {object} [options] - Additional Options
      */
-    constructor(data, fields, take){
+    constructor(data, fields, take, options){
         this.data = data;
         this.fields = fields;
         this.take = take || 10;
@@ -18,18 +22,12 @@ class Grid {
             this.headers = fields.map(x => x.name)
         }
 
-        this.upperBread =`
-    	<table id="c-board-${this.id}" class="c-board c-board-table">
-      	<tbody class="c-board">    
-   	`
-
-        this.lowerBread =`
-				</tbody>
-      </table>
-    `
+        this._table = baseTable
+        this._table.attr("id", `${className}-${this.id}`)
+        this._body = baseBody.clone()
     }
 
-    generateHeaders(){
+    buildHeaders(){
         let th = this.fields || []
         if(this.headers){
             th = this.headers || []
@@ -39,20 +37,26 @@ class Grid {
     }
 
 
-    generatePager() {
-        let d = `<div></div>`
-        if(this.data.length <= this.take) return d
+    buildPager() {
+        let d = $(`<div class="${className} ${className}-pager"></div>`)
+        if(this.data.length <= this.take) {
+            return d
+        }
+
         let div = this.data.length/this.take
         let pages = Math.floor(div)
         if(this.data.length % this.take != 0) pages++
-        let bt = []
         for (let i = 0; i < pages; i++){
-            bt.push(`<button value="${i}" class='c-board c-board-button'>${i+1}`)
+            let b = $(`<button data="${i}" class='c-board c-board-button'>${i+1}</button>`)
+            b.on("click", (e) => {
+                this.pageChange(i)
+            })
+            d.append(b)
         }
-        return `<div>${bt.join("")}</div>`
+        return d
     }
 
-    generateBody() {
+    buildBody() {
         let take = this.data.length - this.skip < this.take ? this.data.length - this.skip : this.take
         let rows = []
         for (let i = this.skip; i < this.skip + take; i++){
@@ -65,12 +69,22 @@ class Grid {
         return rows.join("")
     }
 
-    generate(){
-        return this.upperBread + this.generateHeaders() + this.generateBody() + this.lowerBread + this.generatePager()
+    build(){
+        let component = $(`<div class=${className}></div>`)
+        this._table.append(this.buildInner())
+        component.append(this._table)
+        component.append(this.buildPager())
+        return component
+    }
+
+    buildInner(){
+        this._body = baseBody.clone()
+        this._body.append(this.buildHeaders()).append(this.buildBody())
+        return this._body
     }
 
     reload(){
-        $(`#c-board-${this.id}`).html("<tbody>" + this.generateHeaders() + this.generateBody() + "</tbody>")
+        $(`#c-board-${this.id}`).html(this.buildInner())
     }
 
     pageChange(page){
